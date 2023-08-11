ba511043fbc5b2c2437064642d3e26e63c9d6329
Ali Ghali <ali.ghali@zalando.de>
2023-08-11 20:39:30 +0200

    feat(app): add animated image

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1> Here is the Eatnan app</h1>
+      <img src="https://media.tenor.com/gmXv0eKLZPIAAAAM/pop-corn-eating.gif" alt="eating logo" />
     </div>
   );
 }
