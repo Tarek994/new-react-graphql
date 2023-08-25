ea330963375fe20559519fc98cdc0da70ee72494
Tarek994 <ghali.tarek.123@hotmail.com>
2023-08-25 22:40:47 +0300

    feat(Home.tsx): add the typewriter for the title and text

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
 import "./home.css"
 import hero from "./home-assets/hero5.jpg"
 import hero2 from "./home-assets/hero6.jpg"
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import Typewriter from "typewriter-effect"
 
 const Home = () => {
     return (
         <div id='home' className="home shadow">
             <div className="main-hero ">
+
                 <div className="hero  d-flex">
                 <h1 className="hero-title ">
-                   Eatnan... meet... meal... make company
+                <Typewriter
+                    options={{
+                    strings: 'Eatnan... meet... meal... make company',
+                    autoStart: true,
+                    loop: true,
+                     }}
+                    />
+                   
                     </h1>
                 <h4 className="hero-text ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam repudiandae nulla, quidem quisqua</h4>
                 </div>
@@ -23,7 +30,18 @@ const Home = () => {
             </div>
             <div className="third-part d-flex ">
                 <img src={hero2} alt="" />
-                <h4 className="third-part-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur neque enim nam suscipit sunt, a iure praesentium maiores fugiat aperiam eveniet dignissimos saepe ad vero repellat. Maxime, voluptatibus. Reiciendis, officiis?</h4>
+                
+                
+                <h4 className="third-part-text">
+                <Typewriter
+                    options={{
+                    strings: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur neque enim nam suscipit sunt, a iure praesentium maiores fugiat aperiam eveniet dignissimos saepe ad vero repellat. Maxime, voluptatibus. Reiciendis, officiis?',
+                    autoStart: true,
+                    loop: true,
+                     }}
+                    />
+                    </h4>
+            
             </div>
             <div className="footer">
                 <div className="footer-one">
