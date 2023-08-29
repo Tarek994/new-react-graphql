a1b78b1a8519ed31ac006d33fb820c395e78205c
Tarek994 <ghali.tarek.123@hotmail.com>
2023-08-29 16:09:22 +0300

    feat(Hero.tsx): add the nimated hero text for the hero section

--- src/components/pages/home/hero/Hero.tsx
+++ src/components/pages/home/hero/Hero.tsx
@@ -1,13 +1,21 @@
 import sad2 from "./hero-assets/sad2.jpg"
 import happy from "./hero-assets/happy.jpg"
 import "./hero.css"
+import Typewriter from "typewriter-effect"
 
 const Hero = () => {
     return (
         <div className="hero col-lg-12">
             <div className="hero-text-btn col-lg-5">
                 <p className="hero-text">
-                    538 hours are lost every year to get home made meals. Imaging investing this time in building connections that will change your life, That’s what Eatnan is for!
+                    <Typewriter
+                        options={{
+                            strings: ['538 hours are lost every year to get home made meals. Imaging investing this time in building connections that will change your life, That’s what Eatnan is for!'],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+
                 </p>
                 <button className="hero-button">Know More</button>
             </div>
