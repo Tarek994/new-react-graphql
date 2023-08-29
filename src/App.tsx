7026f9a531afc419544c67bdbe2f287ce6c54dce
Ali Ghali <ali.ghali@zalando.de>
2023-08-30 01:09:05 +0200

    feat(app): use custom theme for UI materials

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,23 @@ import About from "./components/pages/about/About";
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2';
 
-const MainGrid = styled(Grid)(({ theme }) => ({
-  backgroundColor: 'lightblue'
-}));
-
 
 function App() {
+  const MainGrid = styled(Grid)(({ theme }) => ({
+    backgroundColor: theme.palette.primary.main,
+  }));
+
   return (
     <Router>
       <MainGrid justifyContent="center" xs={12} container maxWidth={'100%'}>
+        <Navbar />
         <Grid maxWidth="lg" spacing={2} xs={12} container>
-          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </Grid>
       </MainGrid>
-
     </Router>
   );
 }
