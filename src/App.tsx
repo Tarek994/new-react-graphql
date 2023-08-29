f1aed2f29ed5339a9ba3445941659ae4965e9c7b
Ali Ghali <ali.ghali@zalando.de>
2023-08-29 23:35:19 +0200

    feat(app): initialize app grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import Home from "./components/pages/home/Home";
+import Home from "./containers/Homepage";
 import About from "./components/pages/about/About";
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Unstable_Grid2';
+
+const MainGrid = styled(Grid)(({ theme }) => ({
+  backgroundColor: 'lightblue'
+}));
+
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <MainGrid justifyContent="center" xs={12} container maxWidth={'100%'}>
+        <Grid maxWidth="lg" spacing={2} xs={12} container>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Grid>
+      </MainGrid>
+
     </Router>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,23 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "./navbar.css";
 import logo from "./nav-assets/logo.png";
+import Grid from '@mui/material/Unstable_Grid2';
 
 const Navbar = () => {
   return (
-    <nav className="main-navbar">
-      <div className='navbar'>
-        <div className="navbar-left">
-          <Link to="/">
-            <img src={logo} alt="Logo" className="logo" />
-          </Link>
+    <Grid xs={12}>
+      <nav className="main-navbar" style={{ backgroundColor: 'red' }}>
+        <div className='navbar'>
+          <div className="navbar-left">
+            <Link to="/">
+              <img src={logo} alt="Logo" className="logo" />
+            </Link>
+          </div>
+          <div className="navbar-right">
+            <Link to="/about">
+              <button className="btn">About</button>
+            </Link>
+          </div>
         </div>
-        <div className="navbar-right">
-          <Link to="/about">
-          <button className="btn">About</button>
-          </Link>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </Grid>
+
   );
 };
 
