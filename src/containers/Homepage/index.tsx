35090901dbf7ac4ff92733a61e6fff95939a6636
Tarek994 <ghali.tarek.123@hotmail.com>
2023-09-01 00:00:49 +0300

    feat(homepage-index.tsx): add the Hero component to the homepage

--- src/containers/Homepage/index.tsx
+++ src/containers/Homepage/index.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import { Grid } from '@mui/material';
+import Hero from '../../components/pages/home/hero/Hero';
 
 
 
 
 const Home = () => {
     return (
-        <Grid xs={12} container style={{ minHeight: '100vh' }}>
-            here is the homepage
+        <Grid xs={12} container style={{ minHeight: '100vh',marginTop:'0.5rem'  }}>
+            <Grid item xs={12} style={{maxWidth: '1200px', margin:'auto' }}> 
+            <Hero/>
+            </Grid>
         </Grid>
     )
 }
