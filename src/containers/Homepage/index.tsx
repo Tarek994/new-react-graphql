269ec34f38e764e35c7ce1e49fbfbb4b0b2f1e6e
Tarek994 <ghali.tarek.123@hotmail.com>
2023-09-01 21:44:39 +0300

    feat(containers-index): add the Cards component to the index.tsx

--- src/containers/Homepage/index.tsx
+++ src/containers/Homepage/index.tsx
@@ -5,6 +5,7 @@ import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrow
 import { useTheme } from '@emotion/react';
 import { Fade, AttentionSeeker } from "react-awesome-reveal";
 import Second from '../../components/pages/home/second/Second';
+import Cards from "../../components/pages/home/cards/Cards";
 
 
 const Home = () => {
@@ -22,7 +23,14 @@ const Home = () => {
             </Grid>
             <Grid item xs={12}>
                 <Second/>
-
+            </Grid>
+            <Grid item xs={12} display='center' justifyContent='center' sx={{ paddingTop: 8, paddingBottom: 8, color: 'primary.dark' }}>
+                <AttentionSeeker effect='shakeY'>
+                    <KeyboardDoubleArrowDownIcon sx={{ fontSize: 'h1.fontSize' }} />
+                </AttentionSeeker>
+            </Grid>
+            <Grid item xs={12}>
+                <Cards/>
             </Grid>
         </Grid >
     )
