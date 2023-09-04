c5c551ee20cb1219b7a0dbf677d963222ea9d7fd
Tarek994 <ghali.tarek.123@hotmail.com>
2023-09-04 23:22:21 +0300

    feat(Footer.tsx): add the final footer layout

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,87 +5,103 @@ import { Grid, Typography } from "@mui/material";
 const Footer = () => {
   return (
     <Grid
+      xs={12}
       container
       style={{
         width: "100%",
-        backgroundColor: "var(--color-primary)",
+        margin: "auto",
+        textAlign: "center",
       }}
     >
-      <Grid xs={12}
+      <Grid xs={11}
         container
         item
         style={{
           maxWidth: "1200px",
           margin: "0.5rem auto",
-          padding: "2rem 0rem",
+          padding: "2rem 1rem",
           alignItems: "center",
+          display: "flex",
+          justifyContent: "space-between",
         }}
 
       >
         <Grid
-          item
-          style={{
-            borderRight: "1px solid #000",
-          }}
-          xs={12}
-          lg={6}
-        >
-          <Link
-            to="section-01"
-            style={{
-              display: "flex",
-              alignItems: "center",
-              fontSize: "2rem",
-            }}
-          >
-            EATNAN
-          </Link>
-          <Typography variant="body2">
-            <i>Learn more about the app by visiting the About page.</i>
-          </Typography>
-        </Grid>
-        <Grid
+          xs={12} sm={12}
+          md={6}
+          container
           item
           style={{
             display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
+            flexDirection: "column",
+            marginBottom: "1rem",
           }}
-          xs={12}
-          lg={6}
         >
-          <ul>
-            <li>
-              <Link
-                to="/"
-                style={{
-                  color: "var(--color-light)",
-                  backgroundColor: "var(--color-dark)",
-                  padding: "0.5rem 1rem",
-                  border: "none",
-                  borderRadius: "var(--border-radius-secondary)",
-                  fontSize: "12px",
-                }}
-              >
-                HOME
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/about"
-                style={{
-                  color: "var(--color-light)",
-                  backgroundColor: "var(--color-dark)",
-                  padding: "0.5rem 1rem",
-                  border: "none",
-                  borderRadius: "var(--border-radius-secondary)",
-                  fontSize: "12px",
-                }}
-              >
-                ABOUT
-              </Link>
-            </li>
-          </ul>
+
+          <Grid item sm={12} xs={12}>
+            <Link
+              to="section-01"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                fontSize: "2rem",
+                fontFamily: 'ADLaM Display',
+                fontWeight: 800
+              }}
+            >
+              EATNAN
+            </Link>
+          </Grid>
+
+          <Grid item container md={12} lg={12} sm={6} xs={12}
+            style={{
+              textAlign: "start",
+            }}>
+            <Typography variant="body2">
+              <i>Learn more about the app by visiting the About page.</i>
+            </Typography>
+          </Grid>
+        </Grid>
+
+        {/* navegation */}
+        <Grid xs={6} sm={6} md={6} lg={6}
+          container
+          item
+          style={{
+            display: "flex",
+            textAlign: "end",
+          }}>
+          <Grid item lg={1} md={2} sm={2} xs={2}
+            style={{ marginRight: "1rem" }}>
+            <Link
+              to="/"
+              style={{
+                padding: "0.5rem 1rem",
+                border: "none",
+                borderRadius: "20px",
+                fontSize: "12px",
+                fontWeight: "800",
+              }}
+            >
+              HOME
+            </Link>
+          </Grid>
+
+          <Grid item lg={1} md={2} sm={2} xs={2}>
+            <Link
+              to="/about"
+              style={{
+                padding: "0.5rem 1rem",
+                border: "none",
+                borderRadius: "20px",
+                fontSize: "12px",
+                fontWeight: "800",
+              }}
+            >
+              ABOUT
+            </Link>
+          </Grid>
+
         </Grid>
       </Grid>
     </Grid>
